96cec3a36540abddace18863b19508170f1a0b9b
Wenger Binning <wengerbinning@163.com>
2020-10-18 15:51:49 +0800

    add and modify some file.

--- time.py
+++ time.py
@@ -8,18 +8,21 @@ URL = "http://time.tianqi.com"
 headers = {
         'User-Agent': "Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0"
         }
-url = request.Request(URL,headers=headers)
-
-res = request.urlopen(url)
-
-html = res.read().decode('utf-8')
-#print(html)
 
 pattern = re.compile('<p id="times">(.*?)</p>')
 
-content = re.search(pattern,html)
+def getTime(url:str=URL,headers:dict=headers,pattern=pattern) -> str:
 
-print(content.group(1))
+        url = request.Request(URL,headers=headers)
+        res = request.urlopen(url)
+        html = res.read().decode('utf-8')
+        content = re.search(pattern,html)
+        return content.group(1)
+
+if __name__ == "__main__":
+        print(getTime())
+
+
 
 
 
